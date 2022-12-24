b01d6a3f10bc0181485083c58c1b8a95bd20efc6
Derek <darmfield2023@gmail.com>
2022-12-23 19:04:59 -0500

    enabled functionality to view distinct axial slices

--- Dataset_Creation/view_data.py
+++ Dataset_Creation/view_data.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 data = np.load('Datasets/extracted_np_slices/coronacases_001/image_data.npy')
-
-plt.imshow(data[100], interpolation='nearest')
+print(data[108])
+plt.imshow(data[108], interpolation='nearest')
 plt.show()
 
