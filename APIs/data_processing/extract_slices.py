59e045c225639e9aab0b66315a48a206ff222596
Derek <darmfield2023@gmail.com>
2022-12-23 19:05:48 -0500

    updated metadata

--- APIs/data_processing/extract_slices.py
+++ APIs/data_processing/extract_slices.py
@@ -1,8 +1,9 @@
 import SimpleITK as sitk  
+import numpy as np
 
 
 def extract_slices(file):
-    itk_image = sitk.ImageFileReader('APIs/temporary.vti')
-    return sitk.GetArrayFromImage(itk_image) 
+    # itk_image = sitk.ImageFileReader('APIs/temporary.vti')
+    # return sitk.GetArrayFromImage(itk_image) 
+    return np.load('../Datasets/extracted_np_slices/coronacases_001/image_data.npy')
 
-print(extract_slices('goof'))
