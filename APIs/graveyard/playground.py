b57a4caca4cce3db253d9cf5b3cf4e978d392275
Derek <darmfield2023@gmail.com>
2022-12-25 23:56:35 -0500

    updated metadata

--- APIs/graveyard/playground.py
+++ APIs/graveyard/playground.py
@@ -2,9 +2,9 @@ import requests
 import os
 import urllib
 
-with open('python/asilbek_dumbass.txt') as f:
+with open('APIs/requirements.txt') as f:
     data = f.readlines()[0]
 
-output = requests.get(os.path.join('http://127.0.0.1:8000/append_ct_dataset/', data))
+output = requests.get(os.path.join('http://127.0.0.1:8000/convert-file/', data))
 
 print(output)
