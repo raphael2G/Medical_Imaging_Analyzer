a1f832506b5347002abebd2bdaf4d07379d34b50
Bek Omonkulov <40507855+Asilbec@users.noreply.github.com>
2023-02-04 17:33:34 -0500

    Delete Dataset_Creation directory

--- Dataset_Creation/view_data.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-import numpy as np
-
-data = np.load('Datasets/extracted_np_slices/coronacases_001/image_data.npy')
-print(data[108])
-plt.imshow(data[108], interpolation='nearest')
-plt.show()
-
